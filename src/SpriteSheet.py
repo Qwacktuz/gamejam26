e474da8f979bbc7119f121ec8c7dda743d3a4458
GamesDoneWr0ng <ask@flaten.no>
2026-02-05 21:40:15 +0100

    SpriteSheet start

---
+++ src/SpriteSheet.py
@@ -0,0 +1,22 @@
+import os
+import pygame as pg
+
+class SpriteSheet:
+    def __init__(self, path: str, width: int, height: int):
+        if not os.path.exists(path):
+            raise FileNotFoundError
+        self.sheet = pg.image.load(path)
+
+        self.images = self.get_images(width, height)
+
+    def get_image(self, state, frame):
+        return self.images[state][frame]
+
+    def load_image(self, state, frame, width, height):
+        image = pg.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0, 0), (frame * width, state * height, width, height))
+        return image
+
+    def get_images(self, width, height):
+        size = self.sheet.get_size()
+        return [[self.load_image(i, j, width, height) for j in range(size[0] // height)] for i in range(size[1] // width)]
--- src/World.py
+++ src/World.py
@@ -1,3 +1,5 @@
+import os.path
+
 import numpy as np
 from src.Camera import Camera
 from src.Entity import Entity
@@ -10,12 +12,12 @@ class World:
         self.entities: list[Entity] = []
         self.objects: list[GameObject] = [] # no entities
 
-        self.objects.append(Platform(np.array([600,600]), np.array([200,200])))
+        self.objects.append(Platform(np.array([600,600]), np.array([200,200]), os.path.join("Assets", "32gridmap.png")))
 
     def update(self, deltaTime: float):
         for entity in self.entities:
             entity.update(deltaTime, self.objects)
 
-    def render(self, camera: Camera):
+    def render(self, camera: Camera, animationFrame: int):
         for i in self.objects + self.entities:
-            i.render(camera)
+            i.render(camera, animationFrame, animationFrame)
